5d9d62ee93a944c29b6c60c8f2924fc2a41ee268
k0ttee <k0ttee@yandex.ru>
2023-02-20 20:50:00 +0300

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,3 +1,7 @@
+################################
+# переходы по истории браузера #
+################################
+
 window.addEventListener('popstate',async() => {
 	await loadArticle();
 });
@@ -6,4 +10,26 @@ window.onpopstate=async() => {
 	await loadArticle();
 }
 
-// разница?
+
+
+
+#################
+# большие числа #
+#################
+
+const limit  = '9223372036854775807'; //предел bigserial (https://postgrespro.ru/docs/postgresql/9.6/datatype-numeric)
+let   invite = location.search.split('invite=')[1];
+
+if( BigInt(invite) > BigInt(limit) ){
+	console.error(invite+' - инвайт превышает BIGSERIAL базы Postgres!!!');
+	return;
+}
+
+if( invite.length === 19 ) {
+	for(let key in invite){
+		if( Number(invite[key]) > Number(limit[key]) ){
+			console.error(invite+' - инвайт превышает BIGSERIAL базы Postgres!!!');
+			return;
+		}
+	}
+}
